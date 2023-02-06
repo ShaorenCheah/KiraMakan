1ef51d632041352629b1f3b29e83f44011b0a17f
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2023-02-06 18:14:22 +0800

    Linked with local Bootstrap and local style.css

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="Bootstrap/bootstrap-5.3.0-alpha1-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="Bootstrap/bootstrap-5.3.0-alpha1-dist/js/bootstrap.bundle.js"></script>
+    <title>Kira Makan</title>
 </head>
 <body>
     
