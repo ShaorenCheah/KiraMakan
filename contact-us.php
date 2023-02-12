76ee32c6b0f66088515b094fefa595ccd3943f3d
Sho Yong Quan <yongquan031106@gmail.com>
2023-02-12 21:14:10 +0800

    updated contact-us.php

---
+++ contact-us.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="contactus">
+            <h1>Contact Us</h1>
+            <p>KiraMakan kickstarted with the primary target of solving the burden of bill calculation when splitting the bill. For more inquiries, kindly get in touch with us for:</p>
+            <ul>
+                <li>FAQs</li>
+                <li>Collaboration</li>
+                <li>Terms and Condition</li>
+            </ul>
+            <i class="fa-solid fa-location-dot"></i><b>40, Jalan Ular Kangmuk, 47000 Petaling Jaya.</b><br>
+            <i class="fa-solid fa-phone"></i><b>012-3456789</b><br>
+            <i class="fa-solid fa-envelope"></i><b>kiramakan@gmail.com</b><br>
+        </div>
+        <div class="inputcontainer">
+            <form action ="" method = "POST" class="inputcont">
+                <input class="input" type="text" name="name" id="name" placeholder="Name">
+                <input class="input" type="text" name="email" id="email" placeholder="Email"> 
+                <input class="input" type="text" name="subject" id="subject" placeholder="Subject"> 
+                <textarea class="text" name="message" id="message" placeholder="Message" rows="5" cols="50"></textarea>
+                <input class = "submit" type = "submit" value = "Submit">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
