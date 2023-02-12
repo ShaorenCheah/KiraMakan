c03d63b630ffb176a00bd46ec2ad9714020290aa
Sho Yong Quan <yongquan031106@gmail.com>
2023-02-13 00:06:51 +0800

    updated footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<section id = "footer">
+  <div class = "footer">
+    <div class ="footer-container-1">
+      <div class = "footer-logo">
+        <img src = "images/KiraMakanLogo.jpg">
+      </div>
+      <div class = "footer-nav">
+        <ul class = "footer-into">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>How it Works</li>
+          <li>Partners</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+    <div class = "footer-borderline">
+      <div class = "footer-container-2">
+        <div class = "footer-icons">
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-whatsapp"></i>
+        </div>
+        <div class = "footer-copyright">
+          <p>Copyright ©️ 2023 KiraMakan</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
