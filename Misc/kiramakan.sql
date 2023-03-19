07f1921ceb4ee3fb36f5ce6e41096b26a2258b97
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2023-03-20 00:34:35 +0800

    Database update

---
+++ Misc/kiramakan.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2023 at 05:32 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kiramakan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `accountID` varchar(5) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `accountType` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customerID` varchar(5) NOT NULL,
+  `customerName` varchar(256) NOT NULL,
+  `accountID` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `menuID` varchar(5) NOT NULL,
+  `restaurantID` varchar(5) NOT NULL,
+  `itemName` varchar(100) NOT NULL,
+  `itemNescription` varchar(255) NOT NULL,
+  `itemPrice` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderID` varchar(5) NOT NULL,
+  `restaurantID` varchar(5) NOT NULL,
+  `customerID` varchar(5) DEFAULT NULL,
+  `orderDate` datetime NOT NULL,
+  `totalPrice` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item_person`
+--
+
+CREATE TABLE `order_item_person` (
+  `oipID` varchar(5) NOT NULL,
+  `menuID` varchar(5) NOT NULL,
+  `opID` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_person`
+--
+
+CREATE TABLE `order_person` (
+  `opID` varchar(5) NOT NULL,
+  `orderID` varchar(5) NOT NULL,
+  `personName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE `restaurants` (
+  `restaurantID` varchar(5) NOT NULL,
+  `restaurantName` varchar(256) NOT NULL,
+  `accountID` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`accountID`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerID`),
+  ADD KEY `accountID` (`accountID`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menuID`),
+  ADD KEY `restaurantID` (`restaurantID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderID`),
+  ADD KEY `restaurantID` (`restaurantID`),
+  ADD KEY `customerID` (`customerID`);
+
+--
+-- Indexes for table `order_item_person`
+--
+ALTER TABLE `order_item_person`
+  ADD PRIMARY KEY (`oipID`),
+  ADD KEY `menuID` (`menuID`),
+  ADD KEY `opID` (`opID`);
+
+--
+-- Indexes for table `order_person`
+--
+ALTER TABLE `order_person`
+  ADD PRIMARY KEY (`opID`),
+  ADD KEY `orderID` (`orderID`);
+
+--
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`restaurantID`),
+  ADD KEY `accountID` (`accountID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `accounts` (`accountID`);
+
+--
+-- Constraints for table `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`restaurantID`) REFERENCES `restaurants` (`restaurantID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`restaurantID`) REFERENCES `restaurants` (`restaurantID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`customerID`) REFERENCES `customers` (`customerID`);
+
+--
+-- Constraints for table `order_item_person`
+--
+ALTER TABLE `order_item_person`
+  ADD CONSTRAINT `order_item_person_ibfk_1` FOREIGN KEY (`menuID`) REFERENCES `menu` (`menuID`),
+  ADD CONSTRAINT `order_item_person_ibfk_2` FOREIGN KEY (`opID`) REFERENCES `order_person` (`opID`);
+
+--
+-- Constraints for table `order_person`
+--
+ALTER TABLE `order_person`
+  ADD CONSTRAINT `order_person_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `orders` (`orderID`);
+
+--
+-- Constraints for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD CONSTRAINT `restaurants_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `accounts` (`accountID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+
+// Define database connection variables
+$host = 'localhost';
+$user = 'username';
+$password = 'password';
+$database = 'kiramakan';
+
+// Create a connection object
+$conn = new mysqli($host, $user, $password, $database);
+
+// Check if the connection was successful
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
