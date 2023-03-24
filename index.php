a9606b3c8c7a80ecdbf10d681b6f8a11f09c30b7
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2023-03-24 20:03:04 +0800

    Updated Header and Footer

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -22,7 +22,7 @@
 
     <!-- Hero -->
     <div id="hero">
-        <div class="container row-container col-12 d-flex justify-content-center align-items-center mt-2 py-3 px-2">
+        <div class="container row-container col-9 d-flex justify-content-center align-items-center mt-2 py-3 px-2">
 
             <!-- Left column -->
             <div class="col-4 d-flex justify-content-center align-items-center px-5">
