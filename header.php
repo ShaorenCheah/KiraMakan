9f06d7adc6749f4f2e22c7fbd61375419254a616
Sho Yong Quan <yongquan031106@gmail.com>
2023-03-24 20:08:22 +0800

    Update header.php
    updated navbar right section for triggering modal popup

--- header.php
+++ header.php
@@ -47,17 +47,34 @@
             </div>
             <!-- Navbar logo right -->
             <div class="col-2 w-auto d-flex align-items-center justify-content-center p-3">
+                <?php if (!isset($_SESSION["email"])) {
+                echo '
+                <!-- Button trigger modal -->
+                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "loginModalToggle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                </svg>
+                </button>
+                ';
+                include 'signUpLoginModal.php';
+                } else {
+                echo '
+                <!-- Button trigger modal -->
                 <button class="btn me-3" type="button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-cart" viewBox="0 0 16 16">
-                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                    </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-cart" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg>
                 </button>
 
-                <button class="btn me-3" type="button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
-                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-                    </svg>
+                <!-- Button trigger modal -->
+                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "logoutModalToggle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                </svg>
                 </button>
+                ';
+                }
+                ?>
             </div>
              <!-- Navbar logo right -->
         </div>
