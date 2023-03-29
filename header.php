52fa32cd48af21e675ff3383946f66d3979c605f
Sho Yong Quan <yongquan031106@gmail.com>
2023-03-29 17:08:26 +0800

    Update header.php
    added a modal trigger button for logOut modal form

--- header.php
+++ header.php
@@ -68,12 +68,13 @@
                 </button>
 
                 <!-- Button trigger modal -->
-                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "#logoutModalToggle">
+                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "#logOutModalToggle">
                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                 </svg>
                 </button>
                 ';
+                    include 'logOutModal.php';
                 }
                 ?>
             </div>
