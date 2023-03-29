82d98bfe133b59c704905eda676191aedad21dd9
Sho Yong Quan <yongquan031106@gmail.com>
2023-03-29 18:23:47 +0800

    Update header.php
    removed redundancy from merge

--- header.php
+++ header.php
@@ -1,98 +1,19 @@
 <nav id="header" class="navbar navbar-expand-lg row p-2 py-4 m-0">
 
-        <!-- Navbar collapse menu button -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- Navbar collapse menu button -->
-
-        <!-- Navbar and Navitems -->
-        <div class="collapse navbar-collapse py-2" id="navbarSupportedContent">
-            <div class="col-7 d-flex justify-content-around align-items-center flex-fill">
-                <ul class="navbar-nav me-25">
-                    <div class="w-auto">
-                        <li class="nav-item p-2 px-4">
-                            <a class="nav-link active fs-4" aria-current="page" href="#">Home</a>
-                        </li>
-                    </div>
-                    <div class="w-auto p-2 px-4">
-                        <li class="nav-item">
-                            <a class="nav-link fs-4" href="#">Menu</a>
-                        </li>
-                    </div>
-                    <div class="w-auto p-2 px-4">
-                        <li class="nav-item">
-                            <a class="nav-link fs-4" href="#">How it works</a>
-                        </li>
-                    </div>
-                    <div class="w-auto p-2 px-4">
-                        <li class="nav-item">
-                            <a class="nav-link fs-4" href="#">Partners</a>
-                        </li>
-                    </div>
-                    <div class="w-auto p-2 px-4">
-                        <li class="nav-item">
-                            <a class="nav-link fs-4" href="#">Contact</a>
-                        </li>
-                    </div>
-                </ul>
-            </div>
-            <!-- Navbar logo right -->
-            <div class="col-2 w-auto d-flex align-items-center justify-content-center p-3">
-                <?php if (!isset($_SESSION["email"])) {
-                    echo '
-                <!-- Button trigger modal -->
-                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "#loginModalToggle">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-                </svg>
-                </button>
-                ';
-                    include 'signUpLoginModal.php';
-                } else {
-                    echo '
-                <!-- Button trigger modal -->
-                <button class="btn me-3" type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-cart" viewBox="0 0 16 16">
-                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                </svg>
-                </button>
-
-                <!-- Button trigger modal -->
-                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "#logOutModalToggle">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-                </svg>
-                </button>
-                ';
-                    include 'logOutModal.php';
-                }
-                ?>
-            </div>
-            <!-- Navbar logo right -->
-=======
-    <!-- Navbar brand -->
-    <div class="col-2 w-auto">
-        <a class="navbar-brand h-auto" href="#">
-            <img src="images/KiraMakanLogoPrimary.jpg" alt="Bootstrap" height="55">
-        </a>
-    </div>
-    <!-- Navbar brand -->
-
     <!-- Navbar collapse menu button -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <!-- Navbar collapse menu button -->
 
     <!-- Navbar and Navitems -->
-    <div class="collapse navbar-collapse col-10 " id="navbarSupportedContent">
-        <div class="d-flex justify-content-around align-items-center flex-fill">
+    <div class="collapse navbar-collapse py-2" id="navbarSupportedContent">
+        <div class="col-7 d-flex justify-content-around align-items-center flex-fill">
             <ul class="navbar-nav me-25">
                 <div class="w-auto">
                     <li class="nav-item p-2 px-4">
-                        <a class="nav-link active fs-4" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link active fs-4" aria-current="page" href="#">Home</a>
                     </li>
                 </div>
                 <div class="w-auto p-2 px-4">
@@ -116,27 +37,47 @@
                     </li>
                 </div>
             </ul>
-
         </div>
         <!-- Navbar logo right -->
-        <div class="w-auto d-flex align-items-center justify-content-center">
-            <button class="btn" type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-cart" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+        <div class="col-2 w-auto d-flex align-items-center justify-content-center p-3">
+            <?php if (!isset($_SESSION["email"])) {
+                echo '
+                <!-- Button trigger modal -->
+                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "#loginModalToggle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                 </svg>
-            </button>
-            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-                Test Cart
-            </button>
+                </button>
+                ';
+                include 'signUpLoginModal.php';
+            } else {
+                echo '
+                <!-- Button trigger modal -->
+                <button class="btn me-3" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-cart" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg>
+                </button>
 
-            <button class="btn" type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-person" viewBox="0 0 16 16">
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                <!-- Button trigger modal -->
+                <button class="btn me-3" type="button" data-bs-toggle = "modal" data-bs-target = "#logOutModalToggle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#0584ff" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                 </svg>
-            </button>
+                </button>
+                ';
+                include 'logOutModal.php';
+            }
+            ?>
         </div>
         <!-- Navbar logo right -->
+
+        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+            aria-controls="offcanvasExample">
+            Test Cart
+        </button>
     </div>
+    <!-- Navbar logo right -->
     <!-- Navbar and Navitems -->
 
 </nav>
