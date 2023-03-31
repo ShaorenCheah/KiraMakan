066e7522ca7ec40b13cc9cc64c744af054f46242
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2023-03-31 15:45:46 +0800

    Update index.php

--- restaurant/index.php
+++ restaurant/index.php
@@ -23,21 +23,39 @@
         <?php include 'header.php'; ?>
     </header>
     <div class="row m-3">
-        <div class="col-md-2 h-auto">
-            <div class="row m-0 d-flex flex-column justify-content-center align-items-center g-4">
+        <div class="col-md-3 h-auto">
+            <div class="row m-0 d-flex flex-column justify-content-center align-items-center g-3">
                 <div class="col-md-12">
-                    Info
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"></p>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-12">
-                    Dashboard 1
-                </div>
-                <div class="col-md-12">
-                    Dashboard 2
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-10 h-auto">
-            <div class="row m-0 d-flex flex-column justify-content-center align-items-center g-4">
+
+        <div class="col-md-9 h-auto">
+            <div class="row m-0 d-flex flex-column justify-content-center align-items-center g-3">
                 <div class="col-md-12">
                     Todays Order:
                 </div>
