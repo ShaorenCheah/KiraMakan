b8f187945014a26d7ec4525f8871d7374ee690ba
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2023-04-04 12:26:54 +0800

    Update orderReceipt.php

--- orderReceipt.php
+++ orderReceipt.php
@@ -87,37 +87,83 @@
                         ?>
                         <div class='d-flex justify-content-between flex-row mt-3'>
                             <?php if ($personName == $_SESSION['name']) {
-                                echo '<div class="col-8"></div>';
+                                echo '<div class="col-7"></div>';
                             } else {
                                 echo '
-                            <div class="d-flex align-items-center col-8">
+                            <div class="d-flex align-items-center col-7">
                                 <button class="btn white-btn d-flex align-items-center justify-content-center" data-bs-target="#emailRecipientModalToggle" data-bs-toggle="modal" id="opID" value="$opID"><i class="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-envelope mb-1" viewBox="0 0 16 16">
                                             <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
                                         </svg></i>Send Receipt
                                 </button>
                             </div>';
-                            } ?>
-                            <div class="d-flex flex-column col-4">
+                            }
+
+                            $net = round($sum * 100) / 100;
+                            $service = $net * 0.1;
+                            $sales = $net * 0.06;
+
+                            $final = $net * 1.16;
+                            $secondDecimal = floor($final * 100) % 10;
+
+                            if ($secondDecimal <= 4) {
+                                $final = floor($final * 10) / 10;
+                            } else {
+                                $final = ceil($final * 10) / 10;
+                            }
+                            $service = number_format($service, 2);
+                            $sales = number_format($sales, 2);
+                            $final = number_format($final, 2);
+
+                            ?>
+                            <div class="d-flex flex-column col-5">
                                 <div class="d-flex flex-row justify-content-between gap-1 mb-1">
                                     <p class="mb-0" style="font-size:14px">Service Tax (10%)</p>
-                                    <p class="mb-0" style="font-size:14px">RM <?= $sum ?></p>
+                                    <p class="mb-0" style="font-size:14px">RM <?= $service ?></p>
                                 </div>
                                 <div class="d-flex flex-row justify-content-between gap-1 mb-1">
                                     <p class="mb-0" style="font-size:14px">Sales Tax (6%)</p>
-                                    <p class="mb-0" style="font-size:14px">RM <?= $sum ?></p>
+                                    <p class="mb-0" style="font-size:14px">RM <?= $sales ?></p>
                                 </div>
                                 <div class="d-flex flex-row justify-content-between gap-1 mb-1">
 
-                                    <h5 class="mb-0"><strong>Subtotal</strong></h5>
-                                    <h5 class="mb-0"><strong>RM <span style="color:var(--orange)"><?= $sum ?></span></strong></h5>
+                                    <h5 class="mb-0"><strong>Subtotal</strong><span class="text-muted" style="font-size:12px"> (rounded price)</span></h5>
+                                    <h5 class="mb-0"><strong>RM <span style="color:var(--orange)"><?= $final ?></span></strong></h5>
 
                                 </div>
                             </div>
                         </div>
-                <?php
+                    <?php
                         echo "<div class='cart-item mb-4'></div>";
                     }
-                    echo "<h5 class='d-flex justify-content-end'>Grand Total : <strong>RM " . $totalPrice . "</strong></h5>";
+
+                    $clean = $totalPrice * 0.84;
+
+                    $net = round($clean * 100) / 100;
+                    $service = $net * 0.1;
+                    $sales = $net * 0.06;
+
+                    $service = number_format($service, 2);
+                    $sales = number_format($sales, 2);
+          
+
+                    ?>
+                    <div class="d-flex flex-column col-12 justify-content-end">
+                        <div class="d-flex flex-row justify-content-between gap-1 mb-1">
+                            <p class="mb-0" style="font-size:14px">Service Tax (10%)</p>
+                            <p class="mb-0" style="font-size:14px">RM <?= $service ?></p>
+                        </div>
+                        <div class="d-flex flex-row justify-content-between gap-1 mb-1">
+                            <p class="mb-0" style="font-size:14px">Sales Tax (6%)</p>
+                            <p class="mb-0" style="font-size:14px">RM <?= $sales ?></p>
+                        </div>
+                        <div class="d-flex flex-row justify-content-between gap-1 mb-1">
+
+                            <h5 class="mb-0"><strong>Grandtotal</strong><span class="text-muted" style="font-size:12px"> (rounded price)</span></h5>
+                            <h5 class="mb-0"><strong>RM <span style="color:var(--orange)"><?= $totalPrice ?></span></strong></h5>
+
+                        </div>
+                    </div>
+                <?php
                 } else {
                     echo "No results found";
                 }
